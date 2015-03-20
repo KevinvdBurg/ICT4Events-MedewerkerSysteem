e5fd6b334221318c28200f7673e21340551557e9
KevinvdBurg <vanderburg.kevin@gmail.com>
2015-03-20 22:00:21 +0100

    save

--- MedewerkerSysteem/MedewerkerSysteem/MederwerkerForm.Designer.cs
+++ MedewerkerSysteem/MedewerkerSysteem/MederwerkerForm.Designer.cs
@@ -28,7 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             // 
             // MederwerkerForm
             // 
@@ -42,6 +50,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
 
