1a53d5e50a2aaa8995d941f6f85833c4cefd8a86
BartMemelink <b.memelink@student.fontys.nl>
2015-04-17 16:31:25 +0200

    DBCampingspot

--- MedewerkerSysteem/MedewerkerSysteem/DBCampingspot.cs
+++ MedewerkerSysteem/MedewerkerSysteem/DBCampingspot.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
 
 namespace MedewerkerSysteem
 {
     class DBCampingspot : Database
     {
+        //public List<CampingSpot> SelectAllSpots()
+        //{
+        //    List<CampingSpot> resultaat = new List<CampingSpot>();
+        //    string sql;
+        //    //
+        //    sql = "select * FROM ;
+        //    //sql = "select * from gebruiker";
+        //    string type = "";
+        //    try
+        //    {
+        //        Connect();
+        //        OracleCommand cmd = new OracleCommand(sql, connection);
+        //        OracleDataReader reader = cmd.ExecuteReader();
+        //        if (reader.HasRows)
+        //        {
+        //            while (reader.Read())
+        //            {
+                        
+        //                resultaat.Add(tempSpot);
+
+        //            }
+        //        }
+        //    }
+        //    catch (OracleException e)
+        //    {
+        //        throw;
+        //    }
+        //    finally
+        //    {
+        //        DisConnect();
+        //    }
+        //    return resultaat;
+        //}
     }
 }
