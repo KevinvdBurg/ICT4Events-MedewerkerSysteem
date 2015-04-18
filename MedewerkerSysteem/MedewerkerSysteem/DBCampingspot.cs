710bc7e42c45c4010a75e04dc55268d57a1a1919
BartMemelink <b.memelink@student.fontys.nl>
2015-04-18 14:46:41 +0200

    update reservevenster added

--- MedewerkerSysteem/MedewerkerSysteem/DBCampingspot.cs
+++ MedewerkerSysteem/MedewerkerSysteem/DBCampingspot.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using Oracle.DataAccess.Client;
@@ -9,38 +10,40 @@ namespace MedewerkerSysteem
 {
     class DBCampingspot : Database
     {
-        //public List<CampingSpot> SelectAllSpots()
-        //{
-        //    List<CampingSpot> resultaat = new List<CampingSpot>();
-        //    string sql;
-        //    //
-        //    sql = "select * FROM ;
-        //    //sql = "select * from gebruiker";
-        //    string type = "";
-        //    try
-        //    {
-        //        Connect();
-        //        OracleCommand cmd = new OracleCommand(sql, connection);
-        //        OracleDataReader reader = cmd.ExecuteReader();
-        //        if (reader.HasRows)
-        //        {
-        //            while (reader.Read())
-        //            {
-                        
-        //                resultaat.Add(tempSpot);
+        public List<CampingSpot> SelectAllSpots()
+        {
+            List<CampingSpot> resultaat = new List<CampingSpot>();
+            string sql;
+            //
+            sql = "select maxpersonen, prijs, details, kampeerplekid from kampeerplekcategorie kpc, kampeerplek kp where kampeerplekcategorieid in (select categorieid from kampeerplek)";
+            //sql = "";
 
-        //            }
-        //        }
-        //    }
-        //    catch (OracleException e)
-        //    {
-        //        throw;
-        //    }
-        //    finally
-        //    {
-        //        DisConnect();
-        //    }
-        //    return resultaat;
-        //}
+
+            string type = "";
+            try
+            {
+                Connect();
+                OracleCommand cmd = new OracleCommand(sql, connection);
+                OracleDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+
+                        resultaat.Add(tempSpot);
+
+                    }
+                }
+            }
+            catch (OracleException e)
+            {
+                throw;
+            }
+            finally
+            {
+                DisConnect();
+            }
+            return resultaat;
+        }
     }
 }
